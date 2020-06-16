f13977f901e63d07127982a5af537092b6d6fb0f
Joaquin Ruiz Luque <joaquinruizluque@gmail.com>
2020-06-16 09:57:09 -0300

    Hello world URI typo

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -7,17 +7,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
-	@GetMapping(path = "/hellow-world")
+	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
 		return "Hello World!";
 	}
 	
-	@GetMapping(path = "/hellow-world-bean")
+	@GetMapping(path = "/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
 		return new HelloWorldBean("Hello World!");
 	}
 	
-	@GetMapping(path = "/hellow-world/path-variable/{name}")
+	@GetMapping(path = "/hello-world/path-variable/{name}")
 	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
 		return new HelloWorldBean(String.format("Hello World, %s", name));
 	}
